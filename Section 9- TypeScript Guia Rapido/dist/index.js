d0914f0ba358a34b0071d42a700ad5824b3a38ec
Marlon Martins <marlon-martins@outlook.com>
2024-05-16 17:21:33 +0200

    101. Arrays

--- Section 9- TypeScript Guia Rapido/dist/index.js
+++ Section 9- TypeScript Guia Rapido/dist/index.js
@@ -7,3 +7,11 @@ function average(num1, num2) {
     return (num1 + num2) / 7;
 }
 isApproved = average(grade1, grade2) > 7;
+// Arrays
+const grades = ["7", "9", "8", "7", "6"];
+function finalGrade(grades) {
+    let sum = 0;
+    grades.forEach(grade => sum += parseFloat(grade));
+    return sum / grades.length;
+}
+console.log(finalGrade(grades));
--- Section 9- TypeScript Guia Rapido/src/index.ts
+++ Section 9- TypeScript Guia Rapido/src/index.ts
@@ -3,8 +3,20 @@ let grade1 = Number("8");
 let grade2 = Number("7");
 let isApproved: boolean;
 
-function average(num1:number, num2: number) {
-     return (num1 + num2) / 7;
+function average(num1: number, num2: number) {
+    return (num1 + num2) / 7;
 }
 
 isApproved = average(grade1, grade2) > 7;
+
+
+// Arrays
+const grades = ["7", "9", "8", "7", "6"];
+
+function finalGrade(grades: string[]) {
+    let sum = 0;
+    grades.forEach(grade => sum += parseFloat(grade));
+    return sum / grades.length;
+}
+
+console.log(finalGrade(grades));
