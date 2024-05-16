da3677914879d5718839be50e08022e2539e31eb
Marlon Martins <marlon-martins@outlook.com>
2024-05-16 17:08:57 +0200

    100. Tipos de Dados Primitivos e Funções

--- Section 9- TypeScript Guia Rapido/dist/index.js
+++ Section 9- TypeScript Guia Rapido/dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 let student = "John";
-function printStudent() {
-    return student.toLowerCase();
+let grade1 = Number("8");
+let grade2 = Number("7");
+let isApproved;
+function average(num1, num2) {
+    return (num1 + num2) / 7;
 }
+isApproved = average(grade1, grade2) > 7;
--- Section 9- TypeScript Guia Rapido/src/index.ts
+++ Section 9- TypeScript Guia Rapido/src/index.ts
@@ -1,5 +1,10 @@
 let student = "John";
+let grade1 = Number("8");
+let grade2 = Number("7");
+let isApproved: boolean;
 
-function printStudent() {
-    return student.toLowerCase();
+function average(num1:number, num2: number) {
+     return (num1 + num2) / 7;
 }
+
+isApproved = average(grade1, grade2) > 7;
