be4b0c9aa7de8a582f9ea38bf4a253fff4194417
Marlon Martins <marlon-martins@outlook.com>
2024-06-05 17:12:31 +0200

    13. Apresentação do Novo Projeto - Gerenciador de Tarefas

---
+++ Section 2- Estilos com React/todo-list-initial/src/views/Home.jsx
@@ -0,0 +1,17 @@
+import Filter from '../components/Filter.jsx';
+import TodosList from '../components/TodosList.jsx';
+
+function Home() {
+
+    return (
+    <>
+        <div className="container">
+            <Filter />
+        
+            <TodosList />
+        </div>
+    </>
+    )
+  }
+  
+  export default Home
