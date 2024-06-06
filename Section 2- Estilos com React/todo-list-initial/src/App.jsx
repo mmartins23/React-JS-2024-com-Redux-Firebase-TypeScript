cf5a171bca8501036a7c53dc37fef6438b940a25
Marlon Martins <marlon-martins@outlook.com>
2024-06-06 14:15:09 +0200

    24. Criação do Context da Aplicação e do Provedor

--- Section 2- Estilos com React/todo-list-initial/src/App.jsx
+++ Section 2- Estilos com React/todo-list-initial/src/App.jsx
@@ -2,16 +2,32 @@ import Header from './components/Header.jsx';
 import Home from './views/Home.jsx';
 import './App.scss';
 
+// Hooks
+import { useState } from 'react';
+
+// Context
+import { TodosContext } from './context/TodosContext.js';
+
+
+const initialTodos = [
+  { id: 0, title: 'Do Groceries', description: 'Buy apples, rice, juice and toilet paper.', isDone: true },
+  { id: 1, title: 'Study React', description: 'Understand context & reducers.', isDone: false },
+  { id: 2, title: 'Learn Redux', description: 'Learn state management with Redux', isDone: false }
+];
+
+
 function App() {
+
+  const [todos, setTodos] = useState(initialTodos);
 
   return (
     <>
       <main>
-        
+        <TodosContext.Provider value={{todos, setTodos}}>
         <Header appName="To-Do List with React" />
 
         <Home />
-
+        </TodosContext.Provider>
       </main>
     </>
   )
--- Section 2- Estilos com React/todo-list-initial/src/components/TodosList.jsx
+++ Section 2- Estilos com React/todo-list-initial/src/components/TodosList.jsx
@@ -4,12 +4,14 @@ import Todo from './Todo.jsx';
 // Hooks
 import { useState } from 'react';
 
+
+const initialTodos = [
+  { id: 0, title: 'Do Groceries', description: 'Buy apples, rice, juice and toilet paper.', isDone: true },
+  { id: 1, title: 'Study React', description: 'Understand context & reducers.', isDone: false },
+  { id: 2, title: 'Learn Redux', description: 'Learn state management with Redux', isDone: false }
+];
+
 function TodosList() {
-  const initialTodos = [
-    { id: 0, title: 'Do Groceries', description: 'Buy apples, rice, juice and toilet paper.', isDone: true },
-    { id: 1, title: 'Study React', description: 'Understand context & reducers.', isDone: false },
-    { id: 2, title: 'Learn Redux', description: 'Learn state management with Redux', isDone: false }
-  ];
 
   const [todos, setTodos] = useState(initialTodos);
 
